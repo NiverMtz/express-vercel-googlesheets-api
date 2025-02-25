e2face227d08f058cc1d74b25787c8e54df5a9e7
NiverMtz <nivermartinezmed@gmail.com>
2025-02-25 00:44:20 -0600

    feat(index) set cors config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const attachAuthClient = require("./src/middleware/googleAuth");
-
+const corsConfig = require("./src/config/corsConfig");
 const routes = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors(corsConfig));
 app.use(attachAuthClient);
 
 app.use("/api", routes);
